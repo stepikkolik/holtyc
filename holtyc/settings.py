0a650a7e445c2d1b1bd19790f53176e999431bb7
Štěpík Kolík <stepikkolik@stepik-light.local>
2022-02-20 17:59:24 +0100

    settings

--- holtyc/settings.py
+++ holtyc/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--y)*i3yxpf(+4jr1dc)uwwkv1z@1m12+1g9)ccp=_zh^dg^jg4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
