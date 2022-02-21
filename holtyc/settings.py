b4bd7aff6dda97c8ed0cf32ea5d7debec56e3c0c
Štěpík Kolík <stepikkolik@stepik-light.local>
2022-02-21 17:00:30 +0100

    idk what now

--- holtyc/settings.py
+++ holtyc/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--y)*i3yxpf(+4jr1dc)uwwkv1z@1m12+1g9)ccp=_zh^dg^jg4'
+# SECRET_KEY = 'django-insecure--y)*i3yxpf(+4jr1dc)uwwkv1z@1m12+1g9)ccp=_zh^dg^jg4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
