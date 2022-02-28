81fcab31769f6b5b9b09d755d5ad0f2173b44443
Štěpík Kolík <stepikkolik@stepik-light.local>
2022-02-28 13:38:31 +0100

    gitignore

--- holtyc/settings.py
+++ holtyc/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--y)*i3yxpf(+4jr1dc)uwwkv1z@1m12+1g9)ccp=_zh^dg^jg4'
+SECRET_KEY = config("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
