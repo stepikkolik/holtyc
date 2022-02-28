dbd741c4bfca3cf9ea7b9ff9bc9caf5265711f0f
Štěpík Kolík <stepikkolik@stepik-light.local>
2022-02-28 13:48:53 +0100

    official commit

--- holtyc/settings.py
+++ holtyc/settings.py
@@ -140,8 +140,8 @@ EMAIL_BAKCEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'stepankolias0@gmail.com'
-EMAIL_HOST_PASSWORD = "usnfofdvbdhlwpqn" 
+EMAIL_HOST_USER = config("EMAIL_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
 
 import django_heroku
 django_heroku.settings(locals())
